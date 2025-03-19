52df3ee1c48c7f0e870c4813b17cbe0363a62c02
SonadowB <165947341+SonadowB@users.noreply.github.com>
2025-03-18 20:09:47 -0400

    Update conexión_basica.js

--- practica2/conexión_basica.js
+++ practica2/conexión_basica.js
@@ -1,1 +1,20 @@
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nombres'
+});
+connection.connect((err) => {
+    if (err) {
+        console.error('Error de conexión:', err);
+        return;
+    }
+    console.log('Conectado a la base de datos');
+});
 
+connection.query('SELECT * FROM estudiantes', (error, results) => {
+    if (error) throw error;
+    console.log(results);
+});
+connection.end(); 
